41f68447503b4e64e5380108b9677fa154e666bc
Rahul Ranjan <rahulranjan0225@gmail.com>
2025-12-28 16:43:51 +0530

    first commit

---
+++ practiceSet/src/Test/FindMissingNumberFromArray.java
@@ -0,0 +1,51 @@
+package Test;
+
+public class FindMissingNumberFromArray {
+    public static void main(String[] args) {
+
+        int arr[] = {1,3,5,2,6,7};
+        missingElementByXOR(arr);
+        missingElement(arr);
+
+    }
+
+    /**
+     * Time Complexity = O(n)
+     *  Space Complexity =O(1)
+     */
+    public static void missingElementByXOR(int []arr){
+        int xor1 = 0;
+        int xor2 = 0;
+        int missing_value =0;
+        int n = arr.length + 1; // because one number is missing
+        // XOR all numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            xor1 ^= i;
+        }
+        // XOR all numbers in array
+        for (int num : arr) {
+            xor2 ^= num;
+        }
+        missing_value = xor1^xor2;
+        System.out.println("missing_value : " + missing_value);
+    }
+
+    /**
+     * Time Complexity = O(n)
+     *  Space Complexity =O(1)
+     *  overflow when n is large
+     * Example: with n ≈ 65,000, (n*(n+1))/2 may exceed int
+     * Must assume numbers start at 1 and are consecutive
+     */
+    public static void missingElement(int arr[]){
+        int n = arr.length + 1; // because one number is missing
+        int sumOfArrayElement = 0;
+        int sumOfArrayElementWithMissingNumber = (n*(n+1))/2;
+        int missingNumber =0;
+        for (int num : arr) {
+            sumOfArrayElement +=num;
+        }
+        missingNumber = sumOfArrayElementWithMissingNumber - sumOfArrayElement;
+        System.out.println("missing number: "+missingNumber);
+    }
+}
---
+++ practiceSet/src/Test/FindUniqueNumberFromArray.java
@@ -0,0 +1,26 @@
+package Test;
+
+public class FindUniqueNumberFromArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,1,4,2};
+        int unickNumber = findUniqueNumber(arr);
+        System.out.println("unique number in array is "+unickNumber);
+
+    }
+
+    /**
+     * Time complicity = O(n)
+     * Space Complicity = O(1)
+     * XOR(^) :- 0^n = n and n^n = 0
+     * @param arr
+     * @return
+     */
+    static int findUniqueNumber(int arr[]){
+        int uniqueNumber =0;
+        for (int i = 0; i < arr.length; i++) {
+            uniqueNumber = uniqueNumber^arr[i];
+        }
+        return uniqueNumber;
+    }
+
+}
